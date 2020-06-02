e089757382024dc73cf8f9b53b1395753c2714a7
Advait Lonkar <lonkaradvait@gmail.com>
2020-06-02 09:53:10 +0530

    Evaluate Expression class separated from Frontend

--- statechart-verification-master/src/dfa/run.sh
+++ statechart-verification-master/src/dfa/run.sh
@@ -1,5 +1,7 @@
-JFLEXPATH=/home/redhood/Desktop/CollegeWork/Semester_6/Programming_Languages/Project/Code
-MYCLASSPATH=/home/redhood/Desktop/CollegeWork/Semester_6/Programming_Languages/Project/Code/junit-4.8.1.jar:./:./classes/:${JFLEXPATH}/cup-11a.jar
+#JFLEXPATH=/home/redhood/Desktop/CollegeWork/Semester_6/Programming_Languages/Project/Code
+#MYCLASSPATH=/home/redhood/Desktop/CollegeWork/Semester_6/Programming_Languages/Project/Code/junit-4.8.1.jar:./:./classes/:${JFLEXPATH}/cup-11a.jar
+JFLEXPATH=/home/advait/Desktop/Programming-Languages/StatechartSimulator/
+MYCLASSPATH=/home/advait/Desktop/Programming-Languages/StatechartSimulator/junit-4.8.1.jar:./:./classes/:${JFLEXPATH}/cup-11a.jar
 if [ $# = 1 ]; then
   echo 1
   testcase=$1
