544e917c75c1f588d01267504e3cbba458145d12
Amogh Johri <32429407+AmoghJohri@users.noreply.github.com>
2020-06-03 15:48:37 +0530

    Added History

--- statechart-verification-master/src/dfa/frontend/FrontEnd.java
+++ statechart-verification-master/src/dfa/frontend/FrontEnd.java
@@ -21,19 +21,17 @@ public class FrontEnd {
 
   private final Parser parser;
 
-  /* Necessary for simulator */
+  // necessary for simulator
   private Statechart statechart = null;
+  public static Map<Declaration, Expression> map = new HashMap<Declaration, Expression>(); //Creating a HashMap for the variable bindings, every variable is identified with its fullVName which is unique to it
+  public static List<Transition> transitions = new ArrayList<Transition>();
+  public static Map<State, Integer>history_map = new HashMap<State, Integer>();
 
-  /* Creating a HashMap for the variable bindings, every variable is identified with its fullVName which is unique to it */
-  public static Map<Declaration, Expression> map = new HashMap<Declaration, Expression>();
-
-  /* Constructors */
-  // Original constructor
+  // constructors
   public FrontEnd(String input) throws FileNotFoundException {
     this.parser = new Parser(new Lexer(new FileReader(input)));
   }
 
-  // Constructor for the simulator
   public FrontEnd(Statechart statechart) throws Exception
   {
     this.parser = null; // for the time being
@@ -47,118 +45,203 @@ public class FrontEnd {
     }
 
   }
-
-  /* Parser */
+  // pasrser
   public Parser getParser() {
     return this.parser;
   }
-
-  /* Populate the created map */
-  // All variables are by default bound to null
-  private void populate()
+  
+  // populate map, all variables are by default bound to null
+  private void populate(State s)
   {
-    for(Declaration d : this.statechart.declarations)
+    populate_state(s);
+    populate_transition(s);
+    if(!s.states.isEmpty())
+    {
+      for(State st : s.states)
+        populate(st);
+    }
+  }
+  
+  private void populate_state(State s)
+  {
+    for(Declaration d : s.declarations)
     {
       map.put(d, null);
     }
-    
   }
-  
 
-  // Gets the atomic-state (initial state) for any state up the hierarchy and executes all the entry statements in the process
+  private void populate_transition(State s)
+  {
+    for(Transition t : s.transitions)
+    {
+      transitions.add(t);
+    }
+  }
+
+  // gets the atomic-state (initial state) for any state up the heirarchy and executes all the entry statements in the process
   private State getAtomicState(State state) throws Exception
   {
-   
-  
     State init = state;
-    try {
-    ExecuteStatement.executeStatement(init.entry);
-    for (State s : init.states)
-    {  
-      ExecuteStatement.executeStatement(s.entry);
-
-      if(s.states.isEmpty())
-      {
-        return s;
-      }
+    try 
+    {
+      ExecuteStatement.executeStatement(init.entry);
+      if(init.states.isEmpty())
+        return init;
+      int index = 0;
+      if(history_map.containsKey(init))
+        index = history_map.get(init);
       else
-        init = s;
+        index = 0;
+      return getAtomicState(state.states.get(index));
     }
+    catch (Exception e)
+    {
+      System.out.println("One of the Execute Statements failed!\n");
     }
-  catch (Exception e)
-  {
-    System.out.println("One of the Execute Statements failed!\n");
-  }
-    // if the state is an atomic state itself
-    return init;
+    return init; // this should not happen
   }
   
-
-  /* Displaying all variables - just to get an idea of the state */
+  // displaying all variables - just to get an idea of the state
   private void displayMap()
   {
     for(Map.Entry<Declaration, Expression> m : map.entrySet()){    
       System.out.println(m.getKey().getFullVName()+": "+m.getValue());    
      }  
   }
+  
+  // 
 
-  /* Simulator function invoked in the constructor */
-  public void simulation() throws Exception
+  // checking whether a state maintains history
+  private boolean hasHistory(State s)
   {
-    try {
-    System.out.println("\n\n\nSimulation...\n\n\n");
-    // populating the map to contain all the variable names, as this is a statically typed langugae, no further additions are required in the map
-    this.populate();
-
-    // to check the sequential simulation
-    Scanner input = new Scanner(System.in);  
-    int counter = 0;
-
-    State curr = (State)statechart;
-    List<Transition> transitions = curr.transitions;
-
-    //Main-loop
-    while(true)
+    for(Declaration d : s.declarations)
     {
-      curr = getAtomicState(curr); // gets to the state where from where the execution begins
-      System.out.println("+--------------------------------------------------+");
-      System.out.println("Initial State Map: ");
-      displayMap();
-      System.out.println();
-
-      // System.out.println("Static Analysis: ");
-      // new ZonotopeAbstractDomain(curr, map); // - uncomment this to run the numerical static analyzer
-
-      input.nextLine();
-      System.out.println("After " + counter + " transition/transitions :-");
-      System.out.println("State: " + curr.getFullName());
-      System.out.println("Map: ");
-
-      for(Transition t : transitions) // sequentially executing very transition (only considering the scenarion where the transition is present in the parent and transition occurs in its child states)
+      if(d.getFullVName().equals(s.getFullName() + ".HISTORY")) // if the HISTORY variable exists
       {
-        if(t.getSource() == curr)
+        if(map.get(d) instanceof BooleanConstant) // if the history variable is a boolean constant
         {
-          //if(((BooleanConstant)evaluateExpression(t.guard)).value) // this needs to be evaluated, as of now assuming it to be a BooleanConstant value
-          if(((BooleanConstant)EvaluateExpression.evaluate(t.guard)).value)
-          {
-            ExecuteStatement.executeStatement(curr.exit);
-            System.out.println("Final State Map: ");
-            displayMap();
-            System.out.println("+--------------------------------------------------+");
-            ExecuteStatement.executeStatement(t.action);
-            curr = t.getDestination();
-            break;
-          }
+          if(((BooleanConstant)map.get(d)).value) // if the history variable is true
+            return true;
         }
       }
-      counter ++;
     }
-   }
-  catch (Exception e)
-  {
-    System.out.println("Simulation Failed!\n"); 
+    return false;
   }
-}
+
+  // setting the current state 
+  private void setCurrent(State parent, State current)
+  {
+    int i = 0;
+    for(State s : parent.states) // getting the index of the current state
+    {
+      if(s.equals(current))
+        break;
+      else
+        i ++;
+    }
+    history_map.put(parent, i);
+  }
+
+
+  // bubbling-up to the lower upper bound
+  private void performTransition(Transition t) throws Exception
+  {
+    try
+    {
+      State source = t.getSource();
+      State destination = t.getDestination();
+      State lub = statechart.lub(source, destination);
+      State current = source;
+
+      while(!current.equals(lub))
+      {
+        ExecuteStatement.executeStatement(current.exit);
+        State currParent = current.getSuperstate();
+        if(hasHistory(currParent))
+        {
+          setCurrent(currParent, current);
+        }
+        current = currParent;
+      }
+      
+      ExecuteStatement.executeStatement(t.action);
+      ArrayList<State> path = new ArrayList<State>();
+      current = destination;
+      while(!current.equals(lub))
+      {
+        path.add(current);
+        current = current.getSuperstate();
+      }
+      int i = path.size() - 1;
+      while(i > 1)
+      {
+        ExecuteStatement.executeStatement(path.get(i).entry);
+        i ++;
+      }
+    }
+    catch (Exception e)
+    {
+      System.out.println("Something wrong with execute statement!\n");
+    }
+  }
+
+  public void simulation() throws Exception
+  {
+    try 
+    {
+      System.out.println("\n\n\nSimulation...\n\n\n");
+      // populating the map to contain all the variable names, as this is a statically typed langugae, no further additions are required in the map
+      this.populate(statechart);
+      System.out.println("Initial Statechart Map: ");
+      displayMap();
+
+      // to check the sequential simulation
+      Scanner input = new Scanner(System.in);  
+      int counter = 0;
+
+      State curr = (State)statechart;
+
+      //Main-loop
+      while(true)
+      {
+        curr = getAtomicState(curr); // gets to the state where from where the execution begins
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("Initial State Map: ");
+        displayMap();
+        System.out.println();
+
+        // System.out.println("Static Analysis: ");
+        // new ZonotopeAbstractDomain(curr, map); // - uncomment this to run the numerical static analyzer
+
+        input.nextLine(); // to break the flow into key-strokes
+
+        System.out.println("After " + counter + " transition/transitions :-");
+        System.out.println("State: " + curr.getFullName());
+
+
+        for(Transition t : transitions)
+        {
+          if(t.getSource().equals(curr))
+          {
+            if(((BooleanConstant)EvaluateExpression.evaluate(t.guard)).value)
+            {
+              performTransition(t);
+              System.out.println("Final State Map: ");
+              displayMap();
+              curr = t.getDestination();
+              System.out.println("+--------------------------------------------------+");
+              break;
+            }
+          }
+        }
+        counter ++;
+      }
+    }
+    catch (Exception e)
+    {
+      System.out.println("Simulation Failed!\n"); 
+    }
+  }
 }
 //https://github.com/AmoghJohri/StatechartSimulator.git
 
